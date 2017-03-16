136d9bd458780f9a95ce8015478e25244a1eb583
antigosu <ant1gosu@yandex.ru>
2017-03-16 22:25:21 +0300

    correct ways to moved files

--- sedenspam.py
+++ sedenspam.py
@@ -152,7 +152,7 @@ class Application(tk.Frame):
             sticky='N' + 'S' + 'E' + 'W'
         )
 
-        spam_base_name = 'spam_base.xlsx'
+        spam_base_name = '.\Templates\spam_base.xlsx'
         self.emails = tk.Entry(
             self,
             fg='black',
@@ -206,7 +206,7 @@ class Application(tk.Frame):
             sticky='N' + 'S' + 'E' + 'W'
         )
 
-        with open('default_text.txt') as default:
+        with open('.\Templates\default_text.txt') as default:
             default_text = default.read()
 
         self.text = tkst.ScrolledText(
@@ -336,7 +336,7 @@ class Application(tk.Frame):
 # no main func cuz i need to ran app every time
 window = tk.Tk()
 window.title('sed-en-spam')
-window.wm_iconbitmap('sedenspam.ico')
+window.wm_iconbitmap('.\Pictures\sedenspam.ico')
 window.configure(background='thistle4')
 app = Application(master=window)
 app.mainloop()
