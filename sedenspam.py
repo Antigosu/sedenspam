837dc4733fd595f0f0b34484bc35e09fbac17e30
antigosu <ant1gosu@yandex.ru>
2017-03-21 00:58:14 +0300

    plus two base classes, refactored first ones

--- sedenspam.py
+++ sedenspam.py
@@ -1,14 +1,21 @@
 import tkinter as tk
 
 
-class MainWindow:
-    def __init__(self, master):
-        self.master = master
-        self.master.title('sedenspam')
-        self.master.geometry('600x300+300+225')
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('sedenspam')
+        self.geometry('600x300+300+225')
+        self.wm_iconbitmap('.\images\sedenspam.ico')
+        self.configure(background='thistle4')
+        MainWindow(self)
 
-        DialogWindow(self.master)
-        self.master.mainloop()
+
+class MainWindow(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.grid()
+        FAQField(self)
 
 
 class DialogWindow:
@@ -18,5 +25,22 @@ class DialogWindow:
         self.slave.geometry('200x150+500+375')
 
 
-root = tk.Tk()
-MainWindow(root)
+class FAQField(tk.Label):
+    def __init__(self, master):
+        super().__init__(master)
+        self['text'] = 'Добро пожаловать в SED-EN-SPAM!!1\n' \
+                       'Рассылка спама теперь еще веселее и продуктивнее.'
+        self['fg'] = 'black',
+        self['bg'] = 'bisque3',
+        self['relief'] = 'raised',
+        self['borderwidth'] = 1
+        self.grid(
+            row=0,
+            column=0,
+            columnspan=3,
+            sticky='N' + 'S' + 'E' + 'W'
+        )
+
+# TODO: main block
+root = Application()
+root.mainloop()
