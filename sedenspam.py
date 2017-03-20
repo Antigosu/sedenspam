d4b16bcc64e0dad11220e7ca6aa9419d59b09ed5
antigosu <ant1gosu@yandex.ru>
2017-03-20 22:16:45 +0300

    added classes of base windows

--- sedenspam.py
+++ sedenspam.py
@@ -1,2 +1,22 @@
 import tkinter as tk
 
+
+class MainWindow:
+    def __init__(self, master):
+        self.master = master
+        self.master.title('sedenspam')
+        self.master.geometry('600x300+300+225')
+
+        DialogWindow(self.master)
+        self.master.mainloop()
+
+
+class DialogWindow:
+    def __init__(self, master):
+        self.slave = tk.Toplevel(master)
+        self.slave.title('sedenspam')
+        self.slave.geometry('200x150+500+375')
+
+
+root = tk.Tk()
+MainWindow(root)
