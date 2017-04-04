d1b1dc2c0ceb6726d882e493d193ae149d26789b
antigosu <ant1gosu@yandex.ru>
2017-04-05 00:06:49 +0300

    added console

--- source/labels.py
+++ source/labels.py
@@ -73,6 +73,31 @@ class StatusLabel(tkinter.Label):
     def change_status(self):
         pass
 
+class ConsoleLabel(tkinter.Label):
+    def __init__(self, master):
+        super().__init__(master)
+        self.text = 'some text'
+        self['text'] = self.text
+        self['fg'] = 'black'
+        self['bg'] = 'tan1'
+        self['relief'] = 'raised'
+        self['borderwidth'] = 1
+
+        self.grid(
+            column=0,
+            row=0,
+            columnspan=2,
+            rowspan=1,
+            padx=5,
+            pady=5,
+            ipadx=5,
+            ipady=5,
+            sticky='WENS'
+        )
+
+    def change_status(self):
+        pass
+
 
 class TemplatePreview(tkinter.Label):
     def __init__(self, master):
