937a96076de8df29813919ac462440d4c6848019
antigosu <ant1gosu@yandex.ru>
2017-07-17 19:16:46 +0300

    for better inheritance

--- source/buttons.py
+++ source/buttons.py
@@ -3,37 +3,29 @@ import source.windows
 
 
 class UserChoiceButton(tkinter.Button):
-    def __init__(self, master, row):
+    def __init__(self, master, column, row):
         super().__init__(master)
-        self.master = master
-        self.path = 'click'
-        self['text'] = self.path,
+        self['text'] = 'click',
         self['fg'] = 'black',
         self['bg'] = 'tan1',
         self['cursor'] = 'pirate',
         self['command'] = self.click
 
         self.grid(
-            column=1,
-            row=row,
-            columnspan=1,
-            rowspan=1,
-            padx=5,
-            pady=5,
-            ipadx=5,
-            ipady=5,
+            column=column, row=row, columnspan=1, rowspan=1, # 1 - x
+            padx=5, pady=5, ipadx=5, ipady=5,
             sticky='WENS'
         )
 
     def click(self):
         # source.windows.DialogWindow(self)
-        self.update_idletasks()
+        self.update_idletasks() # Temporary debug.
         print(self.winfo_y())
         print(self.winfo_x())
 
 
 class SettingsButton(tkinter.Button):
-    def __init__(self, master):
+    def __init__(self, master, column, row):
         super().__init__(master)
         self['text'] = 'sets'
         self['fg'] = 'black',
@@ -42,23 +34,20 @@ class SettingsButton(tkinter.Button):
         self['command'] = self.click
 
         self.grid(
-            column=1,
-            row=0,
-            columnspan=1,
-            rowspan=1,
-            padx=5,
-            pady=5,
-            ipadx=5,
-            ipady=5,
+            column=column, row=row, columnspan=1, rowspan=1, # 1 - 0
+            padx=5, pady=5, ipadx=5, ipady=5,
             sticky='WENS'
         )
 
     def click(self):
-        source.windows.DialogWindow(self)
+        # source.windows.DialogWindow(self)
+        self.update_idletasks() # Temporary debug.
+        print(self.winfo_y())
+        print(self.winfo_x())
 
 
 class OperationButton(tkinter.Button):
-    def __init__(self, master, column=0):
+    def __init__(self, master, column, row):
         super().__init__(master)
         self.path = 'click'
         self['text'] = self.path,
@@ -68,20 +57,16 @@ class OperationButton(tkinter.Button):
         self['command'] = self.click
 
         self.grid(
-            column=column,
-            row=0,
-            columnspan=1,
-            rowspan=2,
-            padx=5,
-            pady=5,
-            ipadx=5,
-            ipady=5,
+            column=column, row=row, columnspan=1, rowspan=2, # x - 0
+            padx=5, pady=5, ipadx=5, ipady=5,
             sticky='WENS'
         )
 
     def click(self):
-        source.windows.DialogWindow(self)
-
+        # source.windows.DialogWindow(self)
+        self.update_idletasks()  # Temporary debug.
+        print(self.winfo_y())
+        print(self.winfo_x())
 
 class YesNoButton(tkinter.Button):
     pass
