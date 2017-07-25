95ab27628ad70ac1526c58cbbbb31b573b4a72ad
antigosu <ant1gosu@yandex.ru>
2017-07-25 19:28:30 +0300

    for future

---
+++ source/settings.py
@@ -0,0 +1,20 @@
+from collections import namedtuple
+
+
+class Settings():
+
+    def __init__(self):
+
+        self.color_scheme = {
+
+            'light': {
+
+            },
+
+            'dark':{
+
+            }
+        }
+
+    def update(self):
+
